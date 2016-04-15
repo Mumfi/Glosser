53463a4e12ebb62e2c6104b60d557a1ef615e46e
Johan <nordholts.johan@gmail.com>
2016-04-15 14:58:26 +0200

    basic interface

--- app/src/main/java/com/example/johann/glosser/MainActivity.java
+++ app/src/main/java/com/example/johann/glosser/MainActivity.java
@@ -1,38 +1,35 @@
 package com.example.johann.glosser;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    Button testButton, createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testButton = (Button) findViewById(R.id.test_button);
+        createButton = (Button) findViewById(R.id.create_button);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+   public void startQuiz(View view) {
+       Intent intent = new Intent(this, StartQuizActivity.class);
+        startActivity(intent);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+   }
 
-        return super.onOptionsItemSelected(item);
+    public void createQuiz(View view) {
+        Intent intent = new Intent(this, createQuizAcivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/johann/glosser/StartQuizActivity.java
@@ -0,0 +1,17 @@
+package com.example.johann.glosser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Johan on 2016-04-15.
+ */
+public class StartQuizActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_quiz);
+    }
+
+
+}
